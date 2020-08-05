a5c81ff250fdeeea128c9705a103665f3b8d1814
caozongying <zongying_cao@163.com>
2020-08-05 22:23:22 +0800

    通过交集获取命令
    Signed-off-by: caozongying <zongying_cao@163.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
-from yaml import load
+from yaml import load, dump
+from modules import copy
 
 def get_config_path ():
     return sys.argv[1]
@@ -7,9 +8,18 @@ def get_config_path ():
 
 def main():
     print(get_config_path())
+    cmd_map = {
+        "copy": copy
+    }
+    cmd_keys = [key for key in cmd_map]
+    print(cmd_keys)
     with open(get_config_path(), 'r', encoding='utf-8') as yaml_file:
-        yaml_content = load(yaml_file)
-        print(yaml_content)
+        configs = load(yaml_file)
+        for config in configs:
+            config_keys = [key for key in config]
+            keys = list(set(cmd_keys).intersection(set(config_keys)))
+            print(keys)
+            print(config[keys[0]])
         
     
 
