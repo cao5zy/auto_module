87fd279af375ca3ed00289a8447e26c61d28ec21
caozongying <zongying_cao@163.com>
2020-08-05 22:29:44 +0800

    优化代码
    Signed-off-by: caozongying <zongying_cao@163.com>

--- src/main.py
+++ src/main.py
@@ -2,24 +2,30 @@ import sys
 from yaml import load, dump
 from modules import copy
 
+cmd_map = {
+    "copy": copy
+}
+
+cmd_keys = [key for key in cmd_map]
+
+def get_cmd_key(config):
+    config_keys = [key for key in config]
+    keys = list(set(cmd_keys).intersection(set(config_keys)))
+    if len(keys) == 0:
+        raise Exception('该命令没有包含在:' + ','.join(config_keys))
+    
+    return keys[0]
+    
 def get_config_path ():
     return sys.argv[1]
 
-
 def main():
     print(get_config_path())
-    cmd_map = {
-        "copy": copy
-    }
-    cmd_keys = [key for key in cmd_map]
     print(cmd_keys)
     with open(get_config_path(), 'r', encoding='utf-8') as yaml_file:
         configs = load(yaml_file)
         for config in configs:
-            config_keys = [key for key in config]
-            keys = list(set(cmd_keys).intersection(set(config_keys)))
-            print(keys)
-            print(config[keys[0]])
+            print(get_cmd_key(config))
         
     
 
