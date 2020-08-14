f2af296452b47bea949a9429ebcc32208e290155
caozongying <zongying_cao@163.com>
2020-08-14 21:40:31 +0800

    添加file处理
    Signed-off-by: caozongying <zongying_cao@163.com>

--- src/modules/file.py
+++ src/modules/file.py
@@ -1,5 +1,8 @@
 # 用于文件或者目录创建，使用说明参考ansible file模块，部分实现了其中的功能
-
+from .util import getdictvalue
 def file(param):
-    pass
+    getval = getdictvalue(param)
+    path = getval('path')
+    state = getval('state')
+    root = getval('root')
     
