6277426881a6df4995e1bf9c04d993b0f481757d
caozongying <zongying_cao@163.com>
2020-08-17 15:45:52 +0800

    重构，能够解析嵌套命令
    Signed-off-by: caozongying <zongying_cao@163.com>

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 from yaml import load, dump
 from modules import copy, file, synchronize
 from os import path
+from os.path import join, dirname, abspath, exists
 
 cmd_map = {
     "copy": copy,
@@ -22,18 +23,32 @@ def get_cmd_key(config):
 def get_config_path ():
     return sys.argv[1] 
 
+def handletasks(taskpath):
+    print('handletasks')
+    print(taskpath)
+    if not exists(taskpath):
+        print('follow task not exists')
+        print(taskpath)
+        return
+    
+    with open(taskpath, 'r', encoding='utf-8') as yaml_file:
+        configs = load(yaml_file)
+        for config in configs['tasks']:
+            if isinstance(config, str):
+                handletasks(join(dirname(abspath(taskpath)), config))
+            else:
+                key = get_cmd_key(config)
+                result = cmd_map[key]({**config[key], **{'root': path.dirname(path.abspath(taskpath))}})
+                if result == None:
+                    print("fail at " + key)
+                    print(config[key])
+                    break;
+    
+
 def main():
     print(get_config_path())
     print(cmd_keys)
-    with open(get_config_path(), 'r', encoding='utf-8') as yaml_file:
-        configs = load(yaml_file)
-        for config in configs:
-            key = get_cmd_key(config)
-            result = cmd_map[key]({**config[key], **{'root': path.dirname(path.abspath(get_config_path()))}})
-            if result == None:
-                print("fail at " + key)
-                print(config[key])
-                break;
+    handletasks(get_config_path())
         
     
 
