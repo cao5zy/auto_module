7e86f4fa436924b5aebaae7f5471259f25054837
alan_cao-admin <cao5zy@163.com>
2020-09-06 12:05:02 +0800

    使用optparse
    Signed-off-by: alan_cao-admin <cao5zy@163.com>

--- src/main.py
+++ src/main.py
@@ -3,11 +3,17 @@ from yaml import load, dump
 from modules import handletasks, cmd_map, watch
 from os import path
 from os.path import join, dirname, abspath, exists
+from optparse import OptionParser 
 # import codecs
 # sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach()) 
 
+parser = OptionParser()
+
 def get_config_path ():
-    return sys.argv[1] 
+    args = parser.parse_args()
+    print('args:')
+    print(args[1][0])
+    return args[1][0] 
 
 def main():
     handletasks({**cmd_map, "watch": watch}, get_config_path())
